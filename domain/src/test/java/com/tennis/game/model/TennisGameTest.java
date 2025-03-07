e378f9cb31e40e430a40935efef0b5d65ac7ae46
omakhlas <omarmakhlas@gmail.com>
2025-03-07 18:36:58 +0000

    C3-3 implement getScore function

--- domain/src/test/java/com/tennis/game/model/TennisGameTest.java
+++ domain/src/test/java/com/tennis/game/model/TennisGameTest.java
@@ -1,6 +1,7 @@
 package com.tennis.game.model;
 
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class TennisGameTest {
@@ -8,6 +9,7 @@ public class TennisGameTest {
     @Test
     public void should_test_initial_score_when_the_game_starts() {
         TennisGame game = new TennisGame();
-        var score = game.getScore;
+        var score = game.getScore();
+        Assertions.assertThat(score).isEqualTo("Player A : 0 / Player B : 0");
     }
 }
