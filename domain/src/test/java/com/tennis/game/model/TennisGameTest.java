fd69cd0c752c79c1c36f8845280fcb6221e3de5a
omakhlas <omarmakhlas@gmail.com>
2025-03-07 18:08:07 +0000

    C3-1 add a failing test for the initial score

---
+++ domain/src/test/java/com/tennis/game/model/TennisGameTest.java
@@ -0,0 +1,12 @@
+package com.tennis.game.model;
+
+
+import org.junit.jupiter.api.Test;
+
+public class TennisGameTest {
+
+    @Test
+    public void should_test_initial_score_when_the_game_starts() {
+        TennisGame game = new TennisGame();
+    }
+}
