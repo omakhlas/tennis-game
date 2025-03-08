9814292f287c2d78a71b61116243991f2576b39a
omakhlas <omarmakhlas@gmail.com>
2025-03-08 12:42:42 +0000

    C5-2 refactor code when playerA wins a normal game

--- domain/src/main/java/com/tennis/game/model/TennisGame.java
+++ domain/src/main/java/com/tennis/game/model/TennisGame.java
@@ -6,6 +6,7 @@ import lombok.Getter;
 public class TennisGame {
     private final Player playerA;
     private final Player playerB;
+    private Player winner;
 
     public TennisGame() {
         this.playerA = new Player("A");
@@ -13,10 +14,13 @@ public class TennisGame {
     }
 
     public String getScore() {
+        if(winner != null) return "Player " + winner.getName() + " wins the game";
         return "Player A : " + playerA.getScore().getPoints() + " / Player B : " + playerB.getScore().getPoints();
     }
 
     void pointScoredBy(Player player) {
+        if(winner != null) return;
+        if(player.getScore() == Score.FORTY) winner = player;
         player.nextScore();
     }
 
