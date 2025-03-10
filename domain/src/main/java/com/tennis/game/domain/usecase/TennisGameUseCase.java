2e730a7bd846a97bff8b070e66398b8142d96ae0
omakhlas <omarmakhlas@gmail.com>
2025-03-10 12:04:58 +0000

    C10 add readScores consumer

--- domain/src/main/java/com/tennis/game/domain/usecase/TennisGameUseCase.java
+++ domain/src/main/java/com/tennis/game/domain/usecase/TennisGameUseCase.java
@@ -1,8 +1,8 @@
 package com.tennis.game.domain.usecase;
 
 import com.tennis.game.domain.DomainService;
-import com.tennis.game.domain.events.TennisScorePublisher;
 import com.tennis.game.domain.events.ScoreUpdatedEvent;
+import com.tennis.game.domain.events.TennisScorePublisher;
 import com.tennis.game.domain.model.Player;
 import com.tennis.game.domain.service.ITennisGameService;
 import lombok.RequiredArgsConstructor;
@@ -22,13 +22,17 @@ public class TennisGameUseCase implements ITennisGameUseCase {
     @Override
     public List<String> playGame(String input) {
         log.info("Starting the game");
+        if (!input.matches("[AB]+")) {
+            throw new IllegalArgumentException("Invalid character : Only 'A' and 'B' are allowed");
+        }
         tennisGameService.resetGame();
         var result = new ArrayList<String>();
         for (char c : input.toCharArray()) {
             Player player = (c == 'A') ? tennisGameService.getPlayerA() : tennisGameService.getPlayerB();
             tennisGameService.pointScoredBy(player);
             String score = tennisGameService.getScore();
-            tennisScorePublisher.publish(new ScoreUpdatedEvent(score));
+            ScoreUpdatedEvent event = new ScoreUpdatedEvent(score);
+            tennisScorePublisher.publish(event);
             if (score != null) {
                 result.add(score);
             }
---
+++ infrastructure/src/main/java/com/tennis/game/adapter/secondary/TennisScoreEventConsumer.java
@@ -0,0 +1,20 @@
+package com.tennis.game.adapter.secondary;
+
+import com.tennis.game.domain.events.ScoreUpdatedEvent;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+import java.time.Duration;
+import java.time.temporal.ChronoUnit;
+
+@Slf4j
+@Component
+public class TennisScoreEventConsumer {
+
+    @KafkaListener(topics = "tennis-scores", groupId = "${spring.application.name}")
+    public void readScores(ScoreUpdatedEvent event) throws InterruptedException {
+        Thread.sleep(Duration.of(2, ChronoUnit.SECONDS));
+        System.out.println( event.score());
+    }
+}
