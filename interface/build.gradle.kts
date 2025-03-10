52cfdad2658714a6b388852254a46c196c6c7685
omakhlas <omarmakhlas@gmail.com>
2025-03-10 14:24:05 +0000

    C10: Replace OpenAPI Generator library with Gradle plugin

--- interface/build.gradle.kts
+++ interface/build.gradle.kts
@@ -1,8 +1,10 @@
+import org.openapitools.generator.gradle.plugin.tasks.GenerateTask
+
 plugins {
     java
     `maven-publish`
+    id("org.openapi.generator") version "7.12.0"
 }
-
 
 repositories {
     mavenCentral()
@@ -33,21 +35,42 @@ sourceSets {
                     .asFile.absolutePath,
             )
         }
-        resources {
-            srcDir("$projectDir/src/main/spec")
-        }
     }
 }
 
-tasks.register<GenerateOpenApiServerTask>("openApiGenerateServer") {
-    this.basePackage.set("com.tennis.game.infra.api")
-    // this.specFile.set(openapiSpecFile.get().asFile)
-    this.specFile.set(file("$projectDir/src/main/spec/tennis-game.yaml"))
-    this.outputDir.set(generatedSourcesDir)
+tasks.register<GenerateTask>("openApiGenerateServer") {
+    inputSpec.set(openapiSpecFile.get().asFile.absolutePath)
+    dependsOn("processResources")
+    apiPackage.set("com.tennis.game.infra.api")
+    generatorName.set("spring")
+    modelPackage.set("com.tennis.game.infra.api.model")
+    modelNameSuffix.set("ApiDto")
+    additionalProperties.set(
+        mapOf(
+            "annotationLibrary" to "none",
+            "documentationProvider" to "none",
+            "serializationLibrary" to "jackson",
+            "hideGenerationTimestamp" to "true",
+            "interfaceOnly" to "true",
+            "useSpringBoot3" to "true",
+            "useTags" to "true",
+            "skipApiUtil" to true,
+            "openApiNullable" to false,
+        )
+    )
+    validateSpec.set(false)
+}
+
+tasks.register<Copy>("copyOpenApiSpec") {
+    from("$projectDir/src/main/spec/tennis-game.yaml")
+    into(layout.buildDirectory.dir("resources/main/META-INF/resources"))
+}
+
+tasks.named("processResources") {
+    dependsOn("copyOpenApiSpec")
 }
 
 // run openApiGenerate before compilation
 tasks.getByName("compileJava") {
     dependsOn("openApiGenerateServer")
 }
-
