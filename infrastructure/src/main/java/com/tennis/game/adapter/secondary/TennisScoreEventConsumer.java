c8435eedd03442b1652b9c3d6788d0e4aea961b9
omakhlas <omarmakhlas@gmail.com>
2025-03-10 16:08:45 +0000

    C11 Add README file with project setup and architecture details

--- infrastructure/src/main/java/com/tennis/game/adapter/secondary/TennisScoreEventConsumer.java
+++ infrastructure/src/main/java/com/tennis/game/adapter/secondary/TennisScoreEventConsumer.java
@@ -15,6 +15,6 @@ public class TennisScoreEventConsumer {
     @KafkaListener(topics = "tennis-scores", groupId = "${spring.application.name}")
     public void readScores(ScoreUpdatedEvent event) throws InterruptedException {
         Thread.sleep(Duration.of(2, ChronoUnit.SECONDS));
-        System.out.println( event.score());
+        log.info(event.score());
     }
 }
