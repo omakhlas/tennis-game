36528deae503c8816594ebac321240b7c2cbc385
omakhlas <omarmakhlas@gmail.com>
2025-03-10 14:31:33 +0000

    C10 add readScores consumer

--- domain/src/main/java/com/tennis/game/domain/usecase/TennisGameUseCase.java
+++ domain/src/main/java/com/tennis/game/domain/usecase/TennisGameUseCase.java
@@ -1,10 +1,10 @@
 package com.tennis.game.domain.usecase;
 
 import com.tennis.game.domain.DomainService;
-import com.tennis.game.domain.events.TennisScorePublisher;
 import com.tennis.game.domain.events.ScoreUpdatedEvent;
+import com.tennis.game.domain.events.TennisScorePublisher;
 import com.tennis.game.domain.model.Player;
-import com.tennis.game.domain.service.ITennisGameService;
+import com.tennis.game.domain.model.TennisGame;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
@@ -16,23 +16,26 @@ import java.util.List;
 @DomainService
 public class TennisGameUseCase implements ITennisGameUseCase {
 
-    private final ITennisGameService tennisGameService;
     private final TennisScorePublisher tennisScorePublisher;
 
     @Override
     public List<String> playGame(String input) {
         log.info("Starting the game");
-        tennisGameService.resetGame();
+        if (!input.matches("[AB]+")) {
+            throw new IllegalArgumentException("Invalid character : Only 'A' and 'B' are allowed");
+        }
+        var tennisGame = new TennisGame();
         var result = new ArrayList<String>();
         for (char c : input.toCharArray()) {
-            Player player = (c == 'A') ? tennisGameService.getPlayerA() : tennisGameService.getPlayerB();
-            tennisGameService.pointScoredBy(player);
-            String score = tennisGameService.getScore();
-            tennisScorePublisher.publish(new ScoreUpdatedEvent(score));
+            Player player = (c == 'A') ? tennisGame.getPlayerA() : tennisGame.getPlayerB();
+            tennisGame.pointScoredBy(player);
+            String score = tennisGame.getScore();
             if (score != null) {
+                log.info("Current score: {}", score);
+                ScoreUpdatedEvent event = new ScoreUpdatedEvent(score);
+                tennisScorePublisher.publish(event);
                 result.add(score);
             }
-            log.info("Current score: {}", score);
         }
         return result;
     }
